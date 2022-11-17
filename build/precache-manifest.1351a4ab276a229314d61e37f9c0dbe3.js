8bd19f3a14dae7a4dae922ff2e597601bb90ebd5
fang_shadow <bytor10@live.com>
2022-11-17 05:18:11 -0500

    removed build from .gitignore

---
+++ build/precache-manifest.1351a4ab276a229314d61e37f9c0dbe3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52a45ce3901b5aea1396d25f5cf644f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e2ee3612c48dcb7588",
+    "url": "/static/css/main.9882796f.chunk.css"
+  },
+  {
+    "revision": "b7f7e1ae8283e0a67cd5",
+    "url": "/static/js/2.45c2f8c2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.45c2f8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e2ee3612c48dcb7588",
+    "url": "/static/js/main.aaab96cf.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  }
+]);
